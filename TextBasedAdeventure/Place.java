baa2ac16bcec65a4d1bd727cbc4687a5a1eb11bf
romel <romeljr_acosta@hotmail.com>
2014-02-13 11:25:49 -0800

    game
    games

---
+++ TextBasedAdeventure/Place.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Place here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Place
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Place
+     */
+    public Place()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
